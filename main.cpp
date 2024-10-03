bf9dac3a208d7d7469e3696462e8f39c0c3fca35
kh1dr1 <1rd1hk@proton.me>
2024-10-03 18:37:01 +0300

    2. 3. Projektas atliktas

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
@@ -16,12 +17,14 @@ const float INR_Parduoti = 92.8334;
 
 int main()
 {
+    cout << fixed << setprecision(2);
+
     bool isRunning = true;
     int mainChoice = 0;
 
     while (isRunning)
     {
-        cout << "Pasirinkite funkcija:\n";
+        cout << "\nPasirinkite funkcija:\n";
         cout << "[1] Palyginti valiutu kursus\n";
         cout << "[2] Pirkti valiuta\n";
         cout << "[3] Parduoti valiuta\n";
@@ -38,18 +41,88 @@ int main()
             cout << "Indijos rupija (INR): " << INR_Bendras << "\n\n";
             break;
         case 2:
+        {
+            int choice = 0;
+            int amount = 0;
+
+            cout << "\nPasirinkite valiuta, kuri norite pirkti:\n";
+            cout << "[1] Didziosios Britanijos svaras (GBP)\n";
+            cout << "[2] JAV doleris (USD)\n";
+            cout << "[3] Indijos rupija (INR)\n";
+
+            cout << "\nJusu pasirinkimas:";
+            cin >> choice;
+
+            cout << "Iveskite valiutos kieki:";
+            cin >> amount;
+
+            cout << '\n';
+
+            switch (choice)
+            {
+            case 1:
+                cout << "Jus galite nusipirkti " << amount << " Didziosios Britanijos svaru (GBP) uz " 
+                     << amount / GBP_Pirkti << " euru\n";
+                break;
+            case 2:
+                cout << "Jus galite nusipirkti " << amount << " JAV doleriu (USD) uz " 
+                     << amount / USD_Pirkti << " euru\n";
+                break;
+            case 3:
+                cout << "Jus galite nusipirkti " << amount << " Indijos rupiju (INR) uz " 
+                     << amount / INR_Pirkti << " euru\n";
+                break;
+            default:
+                cout << "Klaida: Neteisingas valiutos pasirinkimas.\n";
+                break;
+            }
 
             break;
-
+        }
         case 3:
+        {
+            int choice = 0;
+            int amount = 0;
+
+            cout << "\nPasirinkite valiuta, kuri norite parduoti:\n";
+            cout << "[1] Didziosios Britanijos svaras (GBP)\n";
+            cout << "[2] JAV doleris (USD)\n";
+            cout << "[3] Indijos rupija (INR)\n";
+
+            cout << "\nJusu pasirinkimas:";
+            cin >> choice;
+
+            cout << "Iveskite valiutos kieki:";
+            cin >> amount;
+
+            cout << '\n';
+
+            switch (choice)
+            {
+            case 1:
+                cout << "Jus galite parduoti " << amount << " Didziosios Britanijos svaru (GBP) uz "
+                     << amount / GBP_Parduoti << " euru\n";
+                break;
+            case 2:
+                cout << "Jus galite parduoti " << amount << " JAV doleriu (USD) uz "
+                     << amount / USD_Parduoti << " euru\n";
+                break;
+            case 3:
+                cout << "Jus galite parduoti " << amount << " Indijos rupiju (INR) uz "
+                     << amount / INR_Parduoti << " euru\n";
+                break;
+            default:
+                cout << "Klaida: Neteisingas valiutos pasirinkimas.\n";
+                break;
+            }
 
             break;
-
+        }
         case 0:
             isRunning = false;
             break;
         default:
-            cout << "Neteisingas pasirinkimas. Bandykite is naujo.\n";
+            cout << "Klaida: Neteisingas pasirinkimas.\n";
             break;
         }
     }
